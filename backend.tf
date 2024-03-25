9bd7e5cb8515dbfafe0f57bb064203a5da8718ce
yahyagulshan <m.yahya.gulshan@gmail.com>
2024-03-26 00:46:35 +0500

    change in pipeline file

--- backend.tf
+++ backend.tf
@@ -4,3 +4,17 @@ terraform {
 		path = "./state/terraform.tfstate"
 	}
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "yahya-backend-234234234234"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    # Optional: Encrypt the state file at rest using a KMS key
+    # encrypt = true
+    # kms_key_id = "arn:aws:kms:your-region:your-account-id:alias/your-kms-alias"
+  }
+}
+
