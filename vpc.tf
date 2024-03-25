43071bc1c5dbce1fc47a0f4c9194428dabb7f5d6
yahyagulshan <m.yahya.gulshan@gmail.com>
2024-03-25 23:41:44 +0500

    add files

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "your-aws-region"
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
 }
+
