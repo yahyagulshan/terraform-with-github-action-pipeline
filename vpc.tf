e525c0ca53476f9c823b3a4b5380b29545382a2c
yahyagulshan <m.yahya.gulshan@gmail.com>
2024-03-26 00:56:41 +0500

    change in pipeline file

--- vpc.tf
+++ vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "staging-vpc-1"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  # azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   # enable_nat_gateway = false
   # enable_vpn_gateway = false
@@ -40,33 +40,33 @@ module "vpc1" {
   name = local.name
   cidr = "20.10.0.0/16" # 10.0.0.0/8 is reserved for EC2-Classic
 
-  azs                 = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
-  public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
-  database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
-  elasticache_subnets = ["20.10.31.0/24", "20.10.32.0/24", "20.10.33.0/24"]
-  redshift_subnets    = ["20.10.41.0/24", "20.10.42.0/24", "20.10.43.0/24"]
-  intra_subnets       = ["20.10.51.0/24", "20.10.52.0/24", "20.10.53.0/24"]
+  # azs                 = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  # private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
+  # public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
+  # database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
+  # elasticache_subnets = ["20.10.31.0/24", "20.10.32.0/24", "20.10.33.0/24"]
+  # redshift_subnets    = ["20.10.41.0/24", "20.10.42.0/24", "20.10.43.0/24"]
+  # intra_subnets       = ["20.10.51.0/24", "20.10.52.0/24", "20.10.53.0/24"]
 
-  create_database_subnet_group = false
+  # create_database_subnet_group = false
 
-  manage_default_network_acl = true
-  default_network_acl_tags   = { Name = "${local.name}-default" }
+  # manage_default_network_acl = true
+  # default_network_acl_tags   = { Name = "${local.name}-default" }
 
-  manage_default_route_table = true
-  default_route_table_tags   = { Name = "${local.name}-default" }
+  # manage_default_route_table = true
+  # default_route_table_tags   = { Name = "${local.name}-default" }
 
-  manage_default_security_group = true
-  default_security_group_tags   = { Name = "${local.name}-default" }
+  # manage_default_security_group = true
+  # default_security_group_tags   = { Name = "${local.name}-default" }
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  # enable_dns_hostnames = true
+  # enable_dns_support   = true
 
-  enable_classiclink             = true
-  enable_classiclink_dns_support = true
+  # enable_classiclink             = true
+  # enable_classiclink_dns_support = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  # enable_nat_gateway = false
+  # single_nat_gateway = false
 
 #   customer_gateways = {
 #     IP1 = {
