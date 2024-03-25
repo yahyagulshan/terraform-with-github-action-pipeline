8dd665bb06633f0eb07d9f874305c25b202331a4
yahyagulshan <m.yahya.gulshan@gmail.com>
2024-03-26 01:14:16 +0500

    change in backend file

--- backend.tf
+++ backend.tf
@@ -1,20 +1,20 @@
-# terraform {
-#   backend "local" {
-
-# 		path = "./state/terraform.tfstate"
-# 	}
-# }
-
-
 terraform {
-  backend "s3" {
-    bucket = "yahya-backend-234234234234"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+  backend "local" {
 
-    # Optional: Encrypt the state file at rest using a KMS key
-    # encrypt = true
-    # kms_key_id = "arn:aws:kms:your-region:your-account-id:alias/your-kms-alias"
-  }
+		path = "./state/terraform.tfstate"
+	}
 }
+
+
+# terraform {
+#   backend "s3" {
+#     bucket = "yahya-backend-234234234234"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+
+#     # Optional: Encrypt the state file at rest using a KMS key
+#     # encrypt = true
+#     # kms_key_id = "arn:aws:kms:your-region:your-account-id:alias/your-kms-alias"
+#   }
+# }
 
